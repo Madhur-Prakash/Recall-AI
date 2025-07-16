42d8b1cf3ad6662de649dbdc41f524563bfbe2c0
Madhur Prakash <madhurprakash2005@gmail.com>
2025-07-17 00:02:50 +0530

    fix: remove trailing space in dotenv import statement

--- backend/recall_ai/src/quad_recall.py
+++ backend/recall_ai/src/quad_recall.py
@@ -5,7 +5,7 @@ from qdrant_client import QdrantClient
 from recall_ai.helpers.dependencies import get_quad_vectorstore, get_embeddings_model, get_llm
 from recall_ai.helpers.utils import setup_logging
 from langchain_core.prompts import ChatPromptTemplate
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 load_dotenv()
 quad_recall = APIRouter()
