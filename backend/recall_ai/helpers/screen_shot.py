3456677accbf5bda4776b53e39d159a9efc2885d
Madhur Prakash <madhurprakash2005@gmail.com>
2025-07-31 22:31:25 +0530

    fix: integrate PaddleOCR for image processing and update requirements

--- backend/recall_ai/helpers/screen_shot.py
+++ backend/recall_ai/helpers/screen_shot.py
@@ -5,7 +5,10 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from helpers.utils import setup_logging
+from paddleocr import PaddleOCR
 
+# Initialize once globally to avoid reloading model every time
+ocr_model = PaddleOCR(use_angle_cls=True, lang='en')  # Use 'en' or 'en+hi' etc.
 logging = setup_logging()
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
@@ -31,4 +34,29 @@ def ocr_image(image_path):
     except Exception as e:
         formatted_error = traceback.format_exc()
         logging.error(f"Error in OCR: {formatted_error}")
+        return None
+
+
+# PaddleOCR
+def ocr_image_paddle(image_path):
+    try:
+        # Read the image using OpenCV
+        image = cv2.imread(image_path)
+        if image is None:
+            raise ValueError("Image not found or unable to read.")
+        logging.info(f"Image loaded successfully from {image_path}")
+
+        # Run OCR using PaddleOCR
+        result = ocr_model.predict(image, cls=True)
+
+        # Extract text and flatten
+        text_list = [line[1][0] for line in result[0]]
+        full_text = " ".join(text_list).strip()
+
+        logging.info("OCR text extracted")
+        return full_text
+
+    except Exception as e:
+        formatted_error = traceback.format_exc()
+        logging.error(f"Error in Paddle OCR: {formatted_error}")
         return None
