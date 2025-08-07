07adf9fbc4b789667cb1a98b4441296cd769f3e8
Madhur Prakash <madhurprakash2005@gmail.com>
2025-08-07 23:18:09 +0530

    Commenetd out paddleOCR as its creating dependency conflict

--- backend/recall_ai/helpers/screen_shot.py
+++ backend/recall_ai/helpers/screen_shot.py
@@ -8,7 +8,7 @@ from helpers.utils import setup_logging
 from paddleocr import PaddleOCR
 
 # Initialize once globally to avoid reloading model every time
-ocr_model = PaddleOCR(use_angle_cls=True, lang='en')  # Use 'en' or 'en+hi' etc.
+# ocr_model = PaddleOCR(use_angle_cls=True, lang='en')  # Use 'en' or 'en+hi' etc.
 logging = setup_logging()
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
@@ -38,25 +38,25 @@ def ocr_image(image_path):
 
 
 # PaddleOCR
-def ocr_image_paddle(image_path):
-    try:
-        # Read the image using OpenCV
-        image = cv2.imread(image_path)
-        if image is None:
-            raise ValueError("Image not found or unable to read.")
-        logging.info(f"Image loaded successfully from {image_path}")
+# def ocr_image_paddle(image_path):
+#     try:
+#         # Read the image using OpenCV
+#         image = cv2.imread(image_path)
+#         if image is None:
+#             raise ValueError("Image not found or unable to read.")
+#         logging.info(f"Image loaded successfully from {image_path}")
 
-        # Run OCR using PaddleOCR
-        result = ocr_model.predict(image, cls=True)
+#         # Run OCR using PaddleOCR
+#         result = ocr_model.predict(image, cls=True)
 
-        # Extract text and flatten
-        text_list = [line[1][0] for line in result[0]]
-        full_text = " ".join(text_list).strip()
+#         # Extract text and flatten
+#         text_list = [line[1][0] for line in result[0]]
+#         full_text = " ".join(text_list).strip()
 
-        logging.info("OCR text extracted")
-        return full_text
+#         logging.info("OCR text extracted")
+#         return full_text
 
-    except Exception as e:
-        formatted_error = traceback.format_exc()
-        logging.error(f"Error in Paddle OCR: {formatted_error}")
-        return None
+#     except Exception as e:
+#         formatted_error = traceback.format_exc()
+#         logging.error(f"Error in Paddle OCR: {formatted_error}")
+#         return None
